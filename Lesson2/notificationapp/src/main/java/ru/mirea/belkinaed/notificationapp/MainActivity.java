86850134f70fcc428c1dcbc9b96ffad281524633
Dealtmin <dealtmin@gmail.com>
2026-04-02 18:58:59 +0300

    Notification

---
+++ Lesson2/notificationapp/src/main/java/ru/mirea/belkinaed/notificationapp/MainActivity.java
@@ -0,0 +1,65 @@
+package ru.mirea.belkinaed.notificationapp;
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(MainActivity.class.getSimpleName().toString(), "Разрешения получены");
+            }
+            else {
+                Log.d(MainActivity.class.getSimpleName().toString(), "Нет разрешений!");
+                int permissionCode = 200;
+                ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS}, permissionCode);
+            }
+        }
+    }
+    public void onClickSendNotification(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("AAAAAAAAAAA!")
+                .setSmallIcon(com.google.android.material.R.drawable.m3_split_button_chevron_avd)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line... Much longer text that cannot fit one line..."))
+                .setContentTitle("MREA");
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Student meow Notification", importance);
+        channel.setDescription("MIREA Channel");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(1, builder.build());
+    }
+}
