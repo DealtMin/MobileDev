4b55ec126ee3982f7070ca586bfd5933e01a9c2c
Dealtmin <dealtmin@gmail.com>
2026-04-14 22:23:25 +0300

    favorite book

--- Lesson3/favoritebook/src/main/java/ru/mirea/belkinaed/favoritebook/MainActivity.java
+++ Lesson3/favoritebook/src/main/java/ru/mirea/belkinaed/favoritebook/MainActivity.java
@@ -3,7 +3,8 @@ package ru.mirea.belkinaed.favoritebook;
 import android.app.Activity;import android.content.Intent;import android.os.Bundle;import android.view.View;import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResult;import androidx.activity.result.ActivityResultCallback;import androidx.activity.result.ActivityResultLauncher;import androidx.activity.result.contract.ActivityResultContracts;import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;import androidx.activity.result.ActivityResultLauncher;import androidx.activity.result.contract.ActivityResultContracts;import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,13 +25,14 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     Intent data = result.getData();
                     String userBook = data.getStringExtra(USER_MESSAGE);
-                    textViewUserBook.setText(userBook);
+                    textViewUserBook.setText(String.format("Название Вашей любимой книги: %s", userBook));
                 }
             }
         };
         activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 callback);
+
     }
     public void getInfoAboutBook(View view) {
         Intent intent = new Intent(this, ShareActivity.class);
--- Lesson3/favoritebook/src/main/java/ru/mirea/belkinaed/favoritebook/ShareActivity.java
+++ Lesson3/favoritebook/src/main/java/ru/mirea/belkinaed/favoritebook/ShareActivity.java
@@ -1,14 +1,24 @@
 package ru.mirea.belkinaed.favoritebook;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ShareActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,19 @@ public class ShareActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            TextView ageView = findViewById(R.id.textView);
+            String university = extras.getString(MainActivity.KEY);
+            ageView.setText(String.format("Любимая книга разработчика – %s", university));
+        }
+    }
+    public void Share(View view){
+        editText = findViewById(R.id.editTextText);
+        String text = editText.getText().toString();
+        Intent data = new Intent();
+        data.putExtra(MainActivity.USER_MESSAGE, text);
+        setResult(Activity.RESULT_OK, data);
+        finish();
     }
 }
