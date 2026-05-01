c588d2f93d9636e5b79418a00c70a4105fabca21
DealtMin <dealtmin@gmail.com>
2026-05-01 22:56:22 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Lesson4/thread/src/main/java/ru/mirea/belkinaed/thread/MainActivity.java
@@ -0,0 +1,79 @@
+package ru.mirea.belkinaed.thread;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Arrays;
+
+import ru.mirea.belkinaed.thread.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private	int	counter	= 0;
+    @Override
+    protected void onCreate(Bundle	savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        TextView infoTextView = findViewById(R.id.textView);
+        Thread mainThread = Thread.currentThread();
+        infoTextView.setText("Имя текущего потока: " + mainThread.getName());
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: XX, НОМЕР ПО СПИСКУ: XX, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: XX");
+        infoTextView.append("\n Новое имя потока: " + mainThread.getName());
+        Log.d(MainActivity.class.getSimpleName(),	"Stack:	"	+	Arrays.toString(mainThread.getStackTrace()));
+        binding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        int numberThread = counter++;
+                        Log.d("ThreadProject", String.format("---Запущен поток № %d студентом группы № %s номер по списку № %d ---", numberThread, "БСБО-09-23", 2));
+                        long endTime = System.currentTimeMillis() + 2 * 1000;
+                        while (System.currentTimeMillis() < endTime) {
+                            synchronized (this) {
+                                try {
+                                    wait(endTime - System.currentTimeMillis());
+                                    Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            Log.d("---ThreadProject", "Выполнен поток № ---" + numberThread);
+                        }
+                    }
+                }).start();
+            }
+        });
+        binding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        int numberThread = counter++;
+                        Log.d("ThreadProject", String.format("---Запущен поток № %d студентом группы № %s номер по списку № %d ---", numberThread, "БСБО-09-23", 2));
+                        long endTime = System.currentTimeMillis() + 2 * 1000;
+                        while (System.currentTimeMillis() < endTime) {
+                            synchronized (this) {
+                                try {
+                                    wait(endTime - System.currentTimeMillis());
+                                    Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            Log.d("---ThreadProject", "Выполнен поток № ---" + numberThread);
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+}
