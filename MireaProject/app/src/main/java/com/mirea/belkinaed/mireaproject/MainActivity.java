113b018ea90e3899ffe86edaaa0ca8882fe5550c
DealtMin <dealtmin@gmail.com>
2026-05-13 09:52:14 +0300

    mirproject 6

--- MireaProject/app/src/main/java/com/mirea/belkinaed/mireaproject/MainActivity.java
+++ MireaProject/app/src/main/java/com/mirea/belkinaed/mireaproject/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home,
+                R.id.nav_profile,
                 R.id.nav_data,
                 R.id.nav_webview,
                 R.id.nav_sensor,
